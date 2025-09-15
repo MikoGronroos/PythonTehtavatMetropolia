d0330d373cc7a6bd4ec432b61ae49e3402ce6510
Miko Gronroos <miko.gronroos@gmail.com>
2025-09-15 17:16:41 +0000

    Done module 8 tasks 2 and 3

--- Moduuli8/Tehtava8.2.py
+++ Moduuli8/Tehtava8.2.py
@@ -1,1 +1,21 @@
-xs:D
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='flight_game',
+        user='root',
+        password='123456789',
+        autocommit=True
+        )
+
+maakoodi = input("Anna maakoodi: ")
+
+sql = f'select count(type), type from airport where iso_country=\"{maakoodi}\" group by type';
+
+kursori = yhteys.cursor()
+kursori.execute(sql)
+
+tulos = kursori.fetchall()
+
+print(tulosmaakoodi)
--- Moduuli8/Tehtava8.3.py
+++ Moduuli8/Tehtava8.3.py
@@ -1,1 +1,24 @@
-:D
+from geopy import distance
+import mysql.connector
+
+icao01 = input("Anna 1 icao: ")
+icao02 = input("Anna 2 icao: ")
+
+yhteys = mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='flight_game',
+        user='root',
+        password='123456789',
+        autocommit=True
+        )
+
+kursori = yhteys.cursor()
+
+def haeAsiat(icao):
+    sql = f'select latitude_deg, longitude_deg from airport where airport.ident = \"{icao}\"';
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return (tulos[0])
+
+print(f'{distance.distance(haeAsiat(icao01), haeAsiat(icao02)).km} km')
