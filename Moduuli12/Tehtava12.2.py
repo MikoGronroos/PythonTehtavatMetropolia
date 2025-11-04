4df8ac95941c33e17f0eb3f4402964099cb16eb2
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-04 18:28:30 +0000

    Add module 12

---
+++ Moduuli12/Tehtava12.2.py
@@ -0,0 +1,7 @@
+import requests
+
+city = input("Enter a city name")
+api_key = "01b379fa6e59e84c0f1ed5ce89135f35"
+pyyntö = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+vastaus = requests.get(pyyntö).json()
+print(vastaus)
